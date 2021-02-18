2fde3aa825bc20d77057790f425af00e108a2f6e
Georgie Horth <georgiehorth@gmail.com>
2021-02-18 16:22:09 +0000

    Added draw_window method which adds colour to the window

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Battle!")
+WHITE = (255, 255, 255)
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
 
 def main():
 
@@ -10,6 +16,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw_window()
+
     pygame.quit()
 
 if __name__ == "__main__":
