b99388e973512d179983db5901edc4b7e92eb8da
Georgie Horth <georgiehorth@gmail.com>
2021-02-19 12:04:31 +0000

    Added FPS. The game run loop will now run 60 times/second or as fast as the computer it's running on can

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Battle!")
 WHITE = (255, 255, 255)
+FPS = 60
 
 def draw_window():
     WIN.fill(WHITE)
     pygame.display.update()
 
 def main():
-
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
